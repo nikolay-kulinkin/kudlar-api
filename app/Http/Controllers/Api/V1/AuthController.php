9772bcff7f8e3e2c0d927444081da55671c47c62
nikolay-kulinkin <kulinkin.nik@gmail.com>
2025-03-09 20:31:37 +0300

    Lesson 9

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+
+class AuthController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        return User::create($request->all());
+    }
+    public function login(LoginUserRequest $request)
+    {
+        if(!Auth::attempt($request->only('email','password'))){
+          return response()->json([
+            'message'=>'Wrong email or password'
+          ],401);  
+        }
+        // $user=Auth::user();
+        $user=User::where('email',$request->email)->first();
+        $user->tokens()->delete();
+        return response()->json([
+            'user'=>$user,
+            'token'=>$user->createToken("Token of user:{$user->name}")->plainTextToken
+        ]);
+    }
+    public function logout()
+    {
+        return 'Logout';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\PostController;
 use App\Http\Controllers\Api\V1\CategoryController;
 
@@ -20,7 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('V1')->middleware('throttle:api')->group(function(){
+    Route::post('register',[AuthController::class,'register']);
+    Route::post('login',[AuthController::class,'login']);
+});
+
 Route::prefix('V1')->middleware(['throttle:api','auth:sanctum'])->group(function(){
     Route::apiResource('categories',CategoryController::class);
     Route::apiResource('posts',PostController::class);
+    Route::get('logout',[AuthController::class,'logout']);
 });
